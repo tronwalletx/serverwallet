488870e879103996cfc8269212963fff45f7f761
Johnsavadkuhi <physitime@gmail.com>
2020-11-09 14:49:54 +0330

    update

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 
+</title> hi <script> alert(3)
+</title> <script> alert(3)</script> <div> 
+</title> hi </script> 
+
+</title>
+<div>
+<div>
+    <div><div><div></div></div></div><div></div><div/></div></div></div><div><div><div><div><div><div><div><div><div>
+        <script>alert(3)</script>
+        
+        
+        
+        <div>
+            <div><div><div><div><div><div><div><div><div><div><div><div><div><div><div><div><div>
+                <div><div><div><div><div><div><div><div><div><div>
+                    <div><div><div><div><div><div><div><div>
+                        <div><div><div><div><div><div><div><div>
+                            <div>
+                                <div><div><div><div>
+                                    <div>
+                                        <div><div><div><div>
+                                            <div> 
+                                                <div><div><div><div><div><div>
+                                                    <div><div><div>
+                                                        <script>console.log(1)</script> 
+                                                        </div></div></div>
+                                                    </div></div></div></div></div></div>
+                                                </div></div></div></div></div>
+                                        </div></div></div></div></div>
+                                        </div></div></div>
+                                                   
+
+
+
+                                        </div></div></div></div></div></div>
+                                                </div></div></div></div></div>
+                                        </div></div></div></div></div>
+                                        </div></div></div>
+                                                    </div></div></div></div></div></div>
+                                                </div></div></div></div></div>
+                                        </div></div></div></div></div>
+
+
